95e21e849afd9506d13c6e318f32ae2cf0d8b372
rommdev <dc.4231253@gmail.com>
2026-06-18 21:20:01 -0500

    just models

---
+++ src/main/java/com/ciberfarma/SpringWebCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ciberfarma/model/Tipo.java
@@ -0,0 +1,9 @@
+package com.ciberfarma.model;
+
+
+
+
+public class Tipo {
+
+	
+}
